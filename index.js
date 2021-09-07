1541895fd53e63b0c805698c265b5c8c301c9462
Jui Hsu <artistray@gmail.com>
2021-09-07 16:38:29 -0400

    commit tree 024d624d87dcf2b16b27d6b53ead628e2b0d94bc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+const currentUser = 'Rtist'
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+const shortGreeting = 'Welcome, ' + currentUser.slice(0,1) + '!'
+
+
+
+
+
+
+
+
+
+
